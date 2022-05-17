184aecb4ae55c68e4c4f8a9fcc847a6a716d9104
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-05-17 16:46:55 -0300

    Agora finalizado

--- Modulo3/Blog/Program.cs
+++ Modulo3/Blog/Program.cs
@@ -10,15 +10,32 @@ namespace Blog // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             var userCotext = new BlogDataContext();
-            userCotext.Users.Add(new User
+            // userCotext.Users.Add(new User
+            // {
+            //     Name = "João Arquiteto",
+            //     Email = "joao24@13.com",
+            //     PassWordHash = "jao29R@",
+            //     Bio = "Casaou e mudou com a mary mary",
+            //     Image = "https:joao.com",
+            //     Slug = "Oi"
+            // });
+            // userCotext.SaveChanges();
+            var user = userCotext.Users.FirstOrDefault();
+            var post = new Post
             {
-                Name = "João Arquiteto",
-                Email = "joao24@13.com",
-                PassWordHash = "jao29R@",
-                Bio = "Casaou e mudou com a mary mary",
-                Image = "https:joao.com",
-                Slug = "Oi"
-            });
+                Author = user,
+                Body = "Meu EFCore",
+                Category = new Category
+                {
+                    Name = "FullStack",
+                    Slug = "Pleno"
+                },
+                CreateDate = System.DateTime.Now,
+                Slug = "Meu artigo",
+                Summary = "Me contratem",
+                Title = "Artigo João",
+            };
+            userCotext.Posts.Add(post);
             userCotext.SaveChanges();
         }
     }
