318b42c08bdb1723522e186c66cb75389fa07f8e
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-05-19 14:05:54 -0300

    Ultimo modulo

---
+++ Modulo5/Blog/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Blog.DataContext;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User
+            {
+                Name = "Mariana",
+                Email = "Mariana@134",
+                PassWordHash = "1234a",
+                Bio = "Linda",
+                Image = "https:mary.com",
+                Slug = "Médica",
+                GitHub = "MaryMary"
+            };
+            var conection = new BlogDataContext();
+            conection.Users.Add(user);
+            conection.SaveChanges();
+        }
+    }
+}
